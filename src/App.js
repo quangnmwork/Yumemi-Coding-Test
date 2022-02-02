4f6f147a82ff5fdb774c36473da234766330f92a
Quang Nguyen <quangnm.work@gmail.com>
2022-02-02 18:11:51 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header";
 import Chart from "./components/Main/Chart/Chart";
 import CheckboxList from "./components/Main/Checkbox/CheckboxList";
+import React from "react";
 
 function App() {
   return (
